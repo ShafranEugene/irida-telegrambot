dfe4a347faad10c9d3bc8958cfc6c5ca09dc8bfc
ShafranEugene <shafranovskyi.yevhenii.cooperate@gmail.com>
2022-03-09 23:33:31 +0200

    [FEATURE] IRIDA-0: create Spring boot project

---
+++ src/main/java/com/github/iridatelegrambot/IridaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.iridatelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IridaTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IridaTelegramBotApplication.class, args);
+	}
+
+}

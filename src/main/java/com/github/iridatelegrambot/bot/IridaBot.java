874e523e2a55e893a5e2a6bbcd00d9662304e1a4
ShafranEugene <shafranovskyi.yevhenii.cooperate@gmail.com>
2022-03-10 22:21:44 +0200

    [FEATURE] IRIDA-0: create Spring boot project

---
+++ src/main/java/com/github/iridatelegrambot/bot/IridaBot.java
@@ -0,0 +1,48 @@
+package com.github.iridatelegrambot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class IridaBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+            SendMessage sendMessage = new SendMessage();
+
+            sendMessage.setChatId(chatId);
+            sendMessage.setText(message);
+
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
